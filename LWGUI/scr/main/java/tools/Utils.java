0bbe9a0ba6dff9852ffc909d1488ae78d04d3780
rober <rober@DESKTOP-FHSN1IG>
2020-06-30 20:11:03 +0100

    Attempt to make rounded edge in panel - it didn't work but introduces soem useful code

--- LWGUI/scr/main/java/tools/Utils.java
+++ LWGUI/scr/main/java/tools/Utils.java
@@ -57,4 +57,42 @@ public class Utils {
 		return output;
 
 	}
+
+	/**
+	 * 
+	 * rounds the edges of the BufferedImage argument based upon the width and
+	 * height of the given values
+	 * 
+	 * @param image        - image to be rounded
+	 * @param cornerRadius - the radius of the rounded corners
+	 * @return - the rounded image
+	 */
+	public static BufferedImage makeRoundedCorner(Image image, int width, int height, int cornerRadius) {
+
+		int w = width;
+		int h = height;
+
+		BufferedImage output = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2 = output.createGraphics();
+
+		// This is what we want, but it only does hard-clipping, i.e. aliasing
+		// g2.setClip(new RoundRectangle2D ...)
+		// so instead fake soft-clipping by first drawing the desired clip shape
+		// in fully opaque white with antialiasing enabled...
+
+		g2.setComposite(AlphaComposite.Src);
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setColor(Color.BLACK);
+		g2.fillRoundRect(0, 0, w, h, cornerRadius, cornerRadius);
+		// g2.fill(new RoundRectangle2D.Float(0, 0, w, h, cornerRadius, cornerRadius));
+
+		// ... then compositing the image on top,
+		// using the white shape from above as alpha source
+		g2.setComposite(AlphaComposite.SrcIn);
+		g2.drawImage(image, 0, 0, null);
+		g2.dispose();
+
+		return output;
+
+	}
 }
